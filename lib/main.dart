528cf7c8f37202f765c28aa801986ae996c2724e
George Leonidis <esentakos@yahoo.gr>
2021-01-26 23:25:45 +0200

    Setups Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:cocktailo/connection/api_connection.dart';
 import 'package:cocktailo/pages/LandingPage.dart';
+import 'package:cocktailo/provider/chosen_cocktail.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 import 'package:splashscreen/splashscreen.dart';
 
 void main() async {
@@ -32,12 +34,15 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return NeumorphicApp(
-      home: SplashScreen(
-        navigateAfterFuture: loadFromFuture(),
-        backgroundColor: const Color(0xff0f1123),
-        imageBackground: Image.asset('assets/loading_bg.png').image,
-        loaderColor: const Color(0xffED1E79),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => ChosenCocktail())],
+      child: NeumorphicApp(
+        home: SplashScreen(
+          navigateAfterFuture: loadFromFuture(),
+          backgroundColor: const Color(0xff0f1123),
+          imageBackground: Image.asset('assets/loading_bg.png').image,
+          loaderColor: const Color(0xffED1E79),
+        ),
       ),
     );
   }
