bb5e46f2a8f29a6ea261f03a484c7744115e27e3
George Leonidis <esentakos@yahoo.gr>
2021-01-28 22:30:22 +0200

    Cleans up landing page

--- lib/pages/LandingPage.dart
+++ lib/pages/LandingPage.dart
@@ -1,10 +1,12 @@
 import 'package:animations/animations.dart';
 import 'package:cocktailo/models/Cocktail.dart';
+import 'package:cocktailo/pages/CocktailPage.dart';
 import 'package:cocktailo/provider/chosen_cocktail.dart';
 import 'package:cocktailo/widgets/Dillema.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:cocktailo/widgets/Menu.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:provider/provider.dart';
 
@@ -68,138 +70,78 @@ class _LandingPageState extends State<LandingPage> {
               child: Menu(),
             ),
             Flexible(
-              flex: 10,
+              flex: 14,
               child: Padding(
                 padding: const EdgeInsets.only(
                   top: 8.0,
                 ),
-                child: ListView.builder(
-                  itemCount: widget.apiResponse.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      height: MediaQuery.of(context).size.height,
-                      width: MediaQuery.of(context).size.width,
-                      child: ListView.builder(
-                        physics: const BouncingScrollPhysics(),
-                        itemCount: cocktailsFirstHalf.length,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: OpenContainer(
-                              closedBuilder: (_, openContainer) => Dilemma(
-                                  leftTitle: cocktailsFirstHalf[index].name,
-                                  onLeftTitleTap: () {
-                                    print('left_cocktail_tapped');
-                                    chosenCocktailState.chosenCocktail =
-                                        cocktailsFirstHalf[index];
-                                    openContainer();
-                                  },
-                                  leftTitleColor: const Color(0xffED1E79),
-                                  width: MediaQuery.of(context).size.width,
-                                  height: 100,
-                                  leftWidget: ExtendedImage.network(
-                                    cocktailsFirstHalf[index].image,
-                                    width: 400,
-                                    height: 400,
-                                    fit: BoxFit.cover,
-                                    cache: true,
-                                    border: Border.all(
-                                      color: Colors.red,
-                                      width: 1.0,
-                                    ),
-                                    borderRadius: const BorderRadius.all(
-                                      Radius.circular(30.0),
-                                    ),
-                                    //cancelToken: CancellationToken(),
-                                  ),
-                                  rightWidget: ExtendedImage.network(
-                                    cocktailsSecondHalf[index].image,
-                                    width: 400,
-                                    height: 400,
-                                    fit: BoxFit.cover,
-                                    cache: true,
-                                    border: Border.all(
-                                      color: Colors.red,
-                                      width: 1.0,
-                                    ),
-                                    borderRadius: const BorderRadius.all(
-                                      Radius.circular(30.0),
-                                    ),
-                                    //cancelToken: CancellationToken(),
-                                  ),
-                                  rightTitleColor: const Color(0xffED1E79),
-                                  rightTitle: cocktailsSecondHalf[index].name,
-                                  onRightTitleTap: () {
-                                    print('right_cocktail_tapped');
-                                    chosenCocktailState.chosenCocktail =
-                                        cocktailsSecondHalf[index];
-                                    openContainer();
-                                  }),
-                              openBuilder: (_, closeContainer) => Scaffold(
-                                appBar: AppBar(
-                                    leading: FlatButton(
-                                  onPressed: closeContainer,
-                                  child: const Icon(
-                                    Icons.backspace,
-                                  ),
-                                )),
-                                body: Column(
-                                  children: [
-                                    ExtendedImage.network(
-                                      chosenCocktailState.chosenCocktail.image,
-                                      fit: BoxFit.fill,
-                                      cache: true,
-                                      border: Border.all(
-                                        color: Colors.red,
-                                        width: 1.0,
-                                      ),
-                                      borderRadius: const BorderRadius.all(
-                                        Radius.circular(30.0),
-                                      ),
-                                      //cancelToken: cancellationToken,
-                                    ),
-                                    Text(
-                                      chosenCocktailState.chosenCocktail.name,
-                                    ),
-                                    Flexible(
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          ...chosenCocktailState
-                                              .chosenCocktail.ingredients
-                                              .map((e) => Flexible(
-                                                    child: Column(
-                                                      children: [
-                                                        Flexible(
-                                                          child: Text(e.name),
-                                                        ),
-                                                        Flexible(
-                                                          child:
-                                                              Text(e.measure),
-                                                        ),
-                                                      ],
-                                                    ),
-                                                  ))
-                                              .toList()
-                                        ],
-                                      ),
-                                    ),
-                                    Flexible(
-                                      child: Text(
-                                        chosenCocktailState
-                                            .chosenCocktail.instructions,
-                                      ),
-                                    ),
-                                  ],
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: ListView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: cocktailsFirstHalf.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: OpenContainer(
+                          closedBuilder: (_, openContainer) => Dilemma(
+                              leftTitle: cocktailsFirstHalf[index].name,
+                              onLeftTitleTap: () {
+                                print('left_cocktail_tapped');
+                                chosenCocktailState.chosenCocktail =
+                                    cocktailsFirstHalf[index];
+                                openContainer();
+                              },
+                              leftTitleColor: const Color(0xffED1E79),
+                              width: MediaQuery.of(context).size.width,
+                              height: 100,
+                              leftWidget: ExtendedImage.network(
+                                cocktailsFirstHalf[index].image,
+                                width: 400,
+                                height: 400,
+                                fit: BoxFit.cover,
+                                cache: true,
+                                border: Border.all(
+                                  color: Colors.red,
+                                  width: 1.0,
                                 ),
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(30.0),
+                                ),
+                                //cancelToken: CancellationToken(),
                               ),
-                            ),
-                          );
-                        },
-                      ),
-                    );
-                  },
+                              rightWidget: ExtendedImage.network(
+                                cocktailsSecondHalf[index].image,
+                                width: 400,
+                                height: 400,
+                                fit: BoxFit.cover,
+                                cache: true,
+                                border: Border.all(
+                                  color: Colors.red,
+                                  width: 1.0,
+                                ),
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(30.0),
+                                ),
+                                //cancelToken: CancellationToken(),
+                              ),
+                              rightTitleColor: const Color(0xffED1E79),
+                              rightTitle: cocktailsSecondHalf[index].name,
+                              onRightTitleTap: () {
+                                print('right_cocktail_tapped');
+                                chosenCocktailState.chosenCocktail =
+                                    cocktailsSecondHalf[index];
+                                openContainer();
+                              }),
+                          openBuilder: (_, closeContainer) => CocktailPage(
+                            cocktail: chosenCocktailState.chosenCocktail,
+                            onBack: closeContainer,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ),
               ),
             ),
