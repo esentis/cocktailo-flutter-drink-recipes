9bac00c786cb1d4b688313d5d501a33fed076941
George Leonidis <esentakos@yahoo.gr>
2021-01-30 21:54:38 +0200

    Cleans up mapper

--- lib/models/Cocktail.dart
+++ lib/models/Cocktail.dart
@@ -15,50 +15,56 @@ class Cocktail {
   /// Method that creates a Cocktail object from Map API response.
   void fromMap(Map<String, dynamic> cocktailMap) {
     id = int.parse(cocktailMap.entries
-        .where((element) => element.key == 'idDrink')
-        .first
+        .firstWhere((element) => element.key == 'idDrink')
         .value
         .toString());
     name = cocktailMap.entries
-        .where((element) => element.key == 'strDrink')
-        .first
+        .firstWhere((element) => element.key == 'strDrink')
         .value
         .toString();
-    category = cocktailMap.entries
-        .where((element) => element.key == 'strCategory')
-        .first
-        .value
-        .toString();
-    glassType = cocktailMap.entries
-        .where((element) => element.key == 'strGlass')
-        .first
-        .value
-        .toString();
+    category = cocktailMap.entries.contains('strCategory')
+        ? cocktailMap.entries
+            .firstWhere((element) => element.key == 'strCategory')
+            .value
+            .toString()
+        : '';
+    glassType = cocktailMap.entries.contains('strGlass')
+        ? cocktailMap.entries
+            .firstWhere((element) => element.key == 'strGlass')
+            .value
+            .toString()
+        : '';
     image = cocktailMap.entries
         .where((element) => element.key == 'strDrinkThumb')
         .first
         .value
         .toString();
-    instructions = cocktailMap.entries
-        .where((element) => element.key == 'strInstructions')
-        .first
-        .value
-        .toString();
-    var ingredientsMapped = cocktailMap.entries
-        .where((element) =>
-            element.key.contains('strIngredient') && element.value != null)
-        .toList();
+    instructions = cocktailMap.entries.contains('strInstructions')
+        ? cocktailMap.entries
+            .where((element) => element.key == 'strInstructions')
+            .first
+            .value
+            .toString()
+        : '';
+    var ingredientsMapped = cocktailMap.entries.contains('strIngredient')
+        ? cocktailMap.entries
+            .where((element) =>
+                element.key.contains('strIngredient') && element.value != null)
+            .toList()
+        : [];
     var measures = cocktailMap.entries
         .where((element) =>
             element.key.contains('strMeasure') && element.value != null)
         .toList();
-    for (var i = 0; i < ingredientsMapped.length; i++) {
-      if (i >= measures.length) {
-        ingredients
-            .add(Ingredient(name: ingredientsMapped[i].value, measure: ''));
-      } else {
-        ingredients.add(Ingredient(
-            name: ingredientsMapped[i].value, measure: measures[i].value));
+    if (ingredientsMapped.isNotEmpty) {
+      for (var i = 0; i < ingredientsMapped.length; i++) {
+        if (i >= measures.length) {
+          ingredients
+              .add(Ingredient(name: ingredientsMapped[i].value, measure: ''));
+        } else {
+          ingredients.add(Ingredient(
+              name: ingredientsMapped[i].value, measure: measures[i].value));
+        }
       }
     }
   }
