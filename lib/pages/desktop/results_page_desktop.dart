bd313b1837442124fa116f9010a3e50493f6e37a
George Leonidis <esentakos@yahoo.gr>
2021-02-07 22:41:49 +0200

    Creates results page for Desktop

---
+++ lib/pages/desktop/results_page_desktop.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResultsPageDesktop extends StatefulWidget {
+  final String ingredient;
+  const ResultsPageDesktop({this.ingredient});
+  @override
+  _ResultsPageDesktopState createState() => _ResultsPageDesktopState();
+}
+
+class _ResultsPageDesktopState extends State<ResultsPageDesktop> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
