17f28bd0ecd3f52cd060ff184c34be48efe6ee0e
George Leonidis <esentakos@yahoo.gr>
2021-02-13 18:54:17 +0200

    Adds animation options

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
+import 'package:auto_animated/auto_animated.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 
@@ -90,4 +91,25 @@ TextStyle kBubbleTextFilled = TextStyle(
       spreadRadius: 5,
     )
   ],
+);
+
+/// Grid items animation options.
+final animatedOptions = const LiveOptions(
+  // Start animation after (default zero)
+  delay: Duration(milliseconds: 300),
+
+  // Show each item through (default 250)
+  showItemInterval: Duration(milliseconds: 200),
+
+  // Animation duration (default 250)
+  showItemDuration: Duration(milliseconds: 300),
+
+  // Animations starts at 0.05 visible
+  // item fraction in sight (default 0.025)
+  visibleFraction: 0.05,
+
+  // Repeat the animation of the appearance
+  // when scrolling in the opposite direction (default false)
+  // To get the effect as in a showcase for ListView, set true
+  reAnimateOnVisibility: false,
 );
