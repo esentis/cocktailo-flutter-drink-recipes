789580a212b8eb2c06d22568f7efe9419097a786
George Leonidis <esentakos@yahoo.gr>
2021-02-20 19:56:43 +0200

    Redesigns mobile cocktail screen

--- lib/pages/mobile/cocktail_page_mobile.dart
+++ lib/pages/mobile/cocktail_page_mobile.dart
@@ -20,6 +20,7 @@ class CocktailPageMobile extends StatefulWidget {
 }
 
 class _CocktailPageMobileState extends State<CocktailPageMobile> {
+  final ScrollController _ingredientsScrollController = ScrollController();
   YoutubePlayerController _controller;
   @override
   void initState() {
@@ -41,98 +42,78 @@ class _CocktailPageMobileState extends State<CocktailPageMobile> {
 
   @override
   Widget build(BuildContext context) {
+    widget.cocktail.ingredients.forEach((element) {
+      kLogger.wtf(element.name);
+    });
     return Scaffold(
       backgroundColor: kColorDarkBlue,
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  bottomRight: Radius.circular(25),
-                  bottomLeft: Radius.circular(25),
-                ),
-                child: ExtendedImage.network(
-                  widget.cocktail.image,
-                  fit: BoxFit.fill,
-                  cache: true,
-                  border: Border.all(
-                    color: Colors.red,
-                    width: 1.0,
-                  ),
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(30.0),
-                  ),
-                  //cancelToken: cancellationToken,
-                ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(60),
+                bottomLeft: Radius.circular(60),
               ),
-              SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 10.0,
-                    vertical: 5,
-                  ),
-                  child: FlatButton(
-                    onPressed: widget.onBack,
-                    padding: const EdgeInsets.all(5),
-                    child: Icon(
-                      Foundation.arrow_left,
-                      size: 35,
-                      color: kColorPink,
+            ),
+            // Display a placeholder widget to visualize the shrinking size.
+            flexibleSpace: Stack(
+              fit: StackFit.expand,
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                      bottomRight: Radius.circular(40),
+                      bottomLeft: Radius.circular(40)),
+                  child: ExtendedImage.network(
+                    widget.cocktail.image,
+                    fit: BoxFit.cover,
+                    cache: true,
+                    border: Border.all(
+                      color: Colors.red,
+                      width: 1.0,
                     ),
+                    //cancelToken: cancellationToken,
                   ),
                 ),
-              )
-            ],
-          ),
-          Text(
-            widget.cocktail.name,
-            style: kBasicStyle.copyWith(
-              fontSize: 22,
+              ],
             ),
-          ),
-          Flexible(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 8.0,
-                horizontal: 10,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ...widget.cocktail.ingredients
-                      .map((e) => Flexible(
-                            child: Column(
-                              children: [
-                                Flexible(
-                                  child: Text(
-                                    e.name,
-                                    style: kBasicStyle.copyWith(
-                                      fontSize: 9,
-                                    ),
-                                  ),
-                                ),
-                                Flexible(
-                                  child: Text(
-                                    e.measure,
-                                    style: kSecondaryStyle.copyWith(
-                                      fontSize: 13,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ))
-                      .toList()
-                ],
+            expandedHeight: MediaQuery.of(context).size.height * .7,
+            toolbarHeight: MediaQuery.of(context).size.height * .5,
+            leading: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                  vertical: 5,
+                ),
+                child: FlatButton(
+                  onPressed: widget.onBack,
+                  padding: const EdgeInsets.all(5),
+                  child: Icon(
+                    Foundation.arrow_left,
+                    size: 35,
+                    color: kColorPink,
+                  ),
+                ),
               ),
             ),
           ),
-          Flexible(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 10.0,
+          SliverToBoxAdapter(
+            child: Center(
+              child: Text(
+                widget.cocktail.name,
+                style: kBasicStyle.copyWith(
+                  fontSize: 22,
+                ),
               ),
+            ),
+          ),
+          const SliverPadding(
+            padding: EdgeInsets.symmetric(
+              vertical: 15,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Center(
               child: Text(
                 widget.cocktail.instructions,
                 textAlign: TextAlign.center,
@@ -142,10 +123,14 @@ class _CocktailPageMobileState extends State<CocktailPageMobile> {
               ),
             ),
           ),
+          const SliverPadding(
+            padding: EdgeInsets.symmetric(
+              vertical: 15,
+            ),
+          ),
           if (widget.cocktail.videoUrl != 'null' &&
               widget.cocktail.videoUrl.isNotEmpty)
-            Flexible(
-              flex: 2,
+            SliverToBoxAdapter(
               child: YoutubePlayer(
                 controller: _controller,
                 bottomActions: [
@@ -155,6 +140,50 @@ class _CocktailPageMobileState extends State<CocktailPageMobile> {
                 ],
               ),
             ),
+          SliverToBoxAdapter(
+            child: Center(
+              child: Text(
+                'Ingredients',
+                style: kBasicStyle.copyWith(
+                  fontSize: 17,
+                ),
+              ),
+            ),
+          ),
+          const SliverPadding(
+            padding: EdgeInsets.symmetric(
+              vertical: 15,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              width: 250,
+              height: 250,
+              child: ListView.builder(
+                itemCount: widget.cocktail.ingredients.length,
+                itemBuilder: (context, index) => ListTile(
+                  title: Text(
+                    widget.cocktail.ingredients[index].name,
+                    style: kSecondaryStyle.copyWith(
+                      fontSize: 17,
+                    ),
+                  ),
+                  subtitle: Text(
+                    widget.cocktail.ingredients[index].measure,
+                    style: kSecondaryStyle.copyWith(
+                      fontSize: 17,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // Text(
+          //   widget.cocktail.name,
+          //   style: kBasicStyle.copyWith(
+          //     fontSize: 22,
+          //   ),
+          // ),
         ],
       ),
     );
